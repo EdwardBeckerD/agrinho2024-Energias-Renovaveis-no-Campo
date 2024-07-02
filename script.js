a207e587db65175633d70338e04f19117693319a
Edward Becker Diniz <edward.diniz@escola.pr.gov.br>
2024-07-02 08:48:03 -0300

    Mudanças no conteúdo da página, e nos botões

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ var bt2 = document.getElementById('bt_2');
 var bt3 = document.getElementById('bt_3');
 var bt4 = document.getElementById('bt_4');
 
-bt1.addEventListener('click',(e)=>{
-    location.href = "./energia-hidreletrica/hidro.html"
+bt1.addEventListener('click',()=>{
+    location.href = "https://brasilescola.uol.com.br/geografia/energia-hidreletrica.htm" /*"./energia-hidreletrica/hidro.html"*/
 })
-bt2.addEventListener('click',(e)=>{
-    location.href = "./energia-decomposicao/bio.html"
+bt2.addEventListener('click',()=>{
+    location.href = "https://brasilescola.uol.com.br/geografia/biocombustiveis.htm" /*"./energia-decomposicao/bio.html"*/
 })
-bt3.addEventListener('click',(e)=>{
-    location.href = "./energia-solar/solar.html"
+bt3.addEventListener('click',()=>{
+    location.href = "https://brasilescola.uol.com.br/geografia/energia-solar.htm" /*"./energia-solar/solar.html"*/
 })
-bt4.addEventListener('click',(e)=>{
-    location.href = "./energia-eolica/vento.html"
+bt4.addEventListener('click',()=>{
+    location.href = "https://brasilescola.uol.com.br/fisica/energia-eolica.htm" /*"./energia-eolica/vento.html"*/
 })

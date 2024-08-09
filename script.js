e2d5047ce046bde213230b5a2c1d8c7bd190d8fa
Edward Becker Diniz <edward.diniz@escola.pr.gov.br>
2024-08-08 23:49:32 -0300

    Mudanças na imagem de fundo dos blocos e na responsividade do site

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ var bt3 = document.getElementById('bt_3');
 var bt4 = document.getElementById('bt_4');
 
 bt1.addEventListener('click',()=>{
-    location.href = "https://brasilescola.uol.com.br/geografia/energia-hidreletrica.htm" /*"./energia-hidreletrica/hidro.html"*/
+    location.href = "https://brasilescola.uol.com.br/geografia/energia-hidreletrica.htm"
 })
 bt2.addEventListener('click',()=>{
-    location.href = "https://brasilescola.uol.com.br/geografia/biocombustiveis.htm" /*"./energia-decomposicao/bio.html"*/
+    location.href = "https://brasilescola.uol.com.br/geografia/biocombustiveis.htm"
 })
 bt3.addEventListener('click',()=>{
-    location.href = "https://brasilescola.uol.com.br/geografia/energia-solar.htm" /*"./energia-solar/solar.html"*/
+    location.href = "https://brasilescola.uol.com.br/geografia/energia-solar.htm"
 })
 bt4.addEventListener('click',()=>{
-    location.href = "https://brasilescola.uol.com.br/fisica/energia-eolica.htm" /*"./energia-eolica/vento.html"*/
+    location.href = "https://brasilescola.uol.com.br/fisica/energia-eolica.htm"
 })
